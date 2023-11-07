042af0c754cff6928b6e9dcffc1bebd49a082a01
Belen Castelli <mariabelencastelli29@gmail.com>
2023-11-07 13:44:47 +0100

    class videogame

---
+++ testVideogame.ts
@@ -0,0 +1,38 @@
+import { Person } from "./person";
+import { Platform } from "./platform";
+import { Videogame } from "./videogame";
+
+let person = new Person("Carlos", "española", "developer", 4)
+let person2 = new Person("David Kazi", "australiana", "director", 7)
+let person3 = new Person("Javier", "española", "tester", 5)
+let person4 = new Person("Paula", "argentina", "manager", 1)
+let person5 = new Person("Paula", "argentina", "manager", 1)
+let person6 = new Person("Carlota", "britanica", "developer", 3)
+let person7 = new Person("Jose", "española", "developer", 9)
+
+let platform = new Platform("PlayStation4",2013, "Sony Interactive Entertaiment", 
+                            64, 8, true, 500, true, "cd", "black")
+let platform2 = new Platform("Xbox One X", 2019, "Microsoft", 64, 9, true, 1000, true,"cd", "black" )
+
+let videogame = new Videogame("Hollow Knight", 2017,[person, person6], "australiana", person2, ["inglés", "español", "francés", "italiano", "japonés"],[platform], 25, 9 )
+
+console.log(videogame.platformExist(platform2));
+console.log(videogame.platformExist(platform));
+
+console.log(videogame.developExist(person6));
+console.log(videogame.developExist(person2));
+console.log(videogame.developExist(person7));
+
+console.log(videogame.languageExist("ruso"));
+console.log(videogame.languageExist("español"));
+
+console.log(videogame.printAll());
+console.log(videogame.toString());
+
+
+
+
+
+
+
+
---
+++ videogame.ts
@@ -0,0 +1,148 @@
+import { Person } from "./person"
+import { Platform } from "./platform"
+class Videogame {
+    private title:string
+    private releaseYear:number
+    private developers:Person[]
+    private nationality:string
+    private director:Person
+    private languages:string[]
+    private platforms:Platform[]
+    private price:number
+    private score: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10
+    // Como simplificarlo
+
+    constructor(title:string, releaseYear:number, developers:Person [], nationality:string,
+                director:Person, languages:string [], platform:Platform[], 
+                price:number, score:  0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10) {
+
+                this.title = title
+                this.releaseYear = releaseYear
+                this.developers = developers
+                this.nationality = nationality
+                this.director = director
+                this.languages = languages
+                this.platforms = platform
+                this.price = price
+                this.score = score
+    }
+
+    public getTitle():string {
+        return this.title
+    }
+
+    public getReleaseYear():number{
+        return this.releaseYear
+    }
+
+    public getDevelopers():Person [] {
+        return this.developers
+    }
+
+    public getNationality():string {
+        return this.nationality
+    }
+
+    public getDirector():Person {
+        return this.director
+    }
+    
+    public getLanguages():string[] {
+        return this.languages
+    }
+
+    public getPlatform():Platform[] {
+        return this.platforms
+    }
+
+    public getPrice():number{
+        return this.price
+    }
+
+    public getScore(): 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10{
+        return this.score
+    }
+
+    public setTitle(newTitle:string): void {
+        this.title = newTitle
+    }
+
+    public setReleaseYear(newReleaseYear:number): void {
+        this.releaseYear = newReleaseYear
+    }
+
+    public setDevelopers(newDevelopers:Person[]): void {
+        this.developers = newDevelopers
+    }
+
+    public setNationality(newNationality:string): void {
+        this.nationality = newNationality
+    }
+
+    public setDirector(newDirector:Person):void {
+        this.director = newDirector
+    }
+
+    public setLanguages(newLanguages:string[]):void {
+        this.languages = newLanguages
+    }
+
+    public setPlatform(newPlatform:Platform[]):void {
+        this.platforms = newPlatform
+    }
+
+    public setPrice(newPrice:number):void {
+        this.price = newPrice
+    }
+
+    public setScore(newScore: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10):void {
+        this.score = newScore
+    }
+    
+// Crear un método que te devuelva verdadero si el videojuego a salido para una
+// plataforma determinada.
+
+    public platformExist(platform:Platform):boolean{
+        let resultado:boolean = false
+        for (let i = 0; i < this.platforms.length; i++) {
+        if(this.platforms[i] == platform) { resultado = true
+            }
+        } return resultado
+    } 
+
+    // Crear un método que te devuelva si un desarrollador ha desarrollado el videojuego.
+
+    public developExist(developer:Person):boolean {
+        let resultado: boolean = false
+       if(developer.getProfession() == "developer"){
+        for (let i = 0; i < this.developers.length; i++) {
+            if(this.developers[i] == developer){resultado = true
+                }
+            }
+        } return resultado
+    }
+    // Crear un método que te devuelva si el videojuego ha sido desarrollado en un idioma
+    // determinado.
+
+    public languageExist(languages:string):boolean{
+        // return this.languages.includes(languages)
+        let resultado:boolean = false
+        for(let lenguas of this.languages){
+        if(lenguas == languages) {resultado = true}
+        } return resultado
+    }
+
+    public printAll ():void {
+        for(let propiedad in this){
+            if(typeof this[propiedad] != "function"){
+                {console.log(`${propiedad}: ${this[propiedad]}`)}
+            }   
+        }
+    }
+
+    public toString():string {
+        let string = ""
+        return string += this.printAll()
+    }
+}
+ export {Videogame}
