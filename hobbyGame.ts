9fbb5ddb3566e063fa70e12312a570e498645bb2
Belen Castelli <mariabelencastelli29@gmail.com>
2023-11-08 12:08:22 +0100

    HobbyGame

---
+++ hobbyGame.ts
@@ -0,0 +1,85 @@
+import { Person } from "./person"
+import { Platform } from "./platform"
+import { Videogame } from "./videogame"
+
+class HobbyGame {
+
+    private videogames: Videogame[]
+
+    constructor(videogames:Videogame[]) {
+        this.videogames = videogames
+    }
+
+    public getVideogames():Videogame[] {
+        return this.videogames
+    }
+
+    public setVideogames(newVideogames:Videogame[]):void{
+        this.videogames = newVideogames
+    }
+    // Crear un método que devuelva el precio total de todos los videojuegos de una plataforma 
+    // determinada
+    public priceTotalPlatform(platform:Platform):number {
+        let resultado:number = 0
+        for(let i = 0; i < this.videogames.length; i++){
+        if(this.videogames[i].getPlatform().includes(platform)) {
+            resultado += this.videogames[i].getPrice()
+            }
+        } return resultado
+    }   
+
+    // Crear un método que devuelva el precio total de los videojuegos de HobbyGame
+
+    public priceTotal():number {
+       let resultado:number = 0
+       for(let videogame of this.videogames) {
+        resultado += videogame.getPrice()
+       }
+       return resultado
+    }
+
+    // Crear un método que devuelva los videojuegos desarrollados en un idioma determinado
+
+    public videogameLanguage(language:string):Videogame[]{
+        let resultado:Videogame [] = []
+        for(let videogame of this.videogames){
+           if (videogame.languageExist(language) == true) {
+            resultado.push(videogame)
+           }
+        } return resultado
+    }
+
+    public videogameLanguage2(language:string):Videogame[]{
+        let videogamefilter:Videogame[] = this.videogames.filter((videogame) => videogame.languageExist(language))
+        return videogamefilter
+    }
+
+    // Crear un método que devuelva los videojuegos desarrollados por un desarrollador
+    // determinado.
+
+    public videogameDevelop(developer:Person): Videogame[]{
+        let videogamefilter:Videogame[] = this.videogames.filter((videogame) => videogame.developExist(developer))
+        return videogamefilter
+    }
+    // Crear un método que te devuelva los videojuegos desarrollados para una determinada 
+    // plataforma y en un determinado idioma
+
+    public videogamePlatLang(platform:Platform, language:string): Videogame[]{
+        let videogamefilter:Videogame[] = this.videogames.filter((videogame) => 
+        videogame.platformExist(platform) && videogame.languageExist(language))
+        return videogamefilter
+    }
+
+    // Crear un método que devuelva todos los videojuegos cuya nota sea mayor o igual a 5
+    
+    public videogameScore():Videogame[]{
+        let videogamefilter:Videogame[] = this.videogames.filter((videojuego) =>
+        videojuego.getScore() >= 5)
+        return videogamefilter
+    }
+}
+
+export {HobbyGame}
+
+
+
---
+++ testHobbyGame.ts
@@ -0,0 +1,41 @@
+import { Person } from "./person";
+import { Platform } from "./platform";
+import { Videogame } from "./videogame";
+import { HobbyGame } from "./hobbyGame";
+
+let person = new Person("Carlos", "española", "developer", 4)
+let person2 = new Person("David Kazi", "australiana", "director", 7)
+let person3 = new Person("Javier", "española", "tester", 5)
+let person4 = new Person("Paula", "argentina", "manager", 1)
+let person5 = new Person("Paula", "argentina", "manager", 1)
+let person6 = new Person("Carlota", "britanica", "developer", 3)
+let person7 = new Person("Jose", "española", "developer", 9)
+
+let platform = new Platform("PlayStation4",2013, "Sony Interactive Entertaiment", 
+                            64, 8, true, 500, true, "cd", "black")
+let platform2 = new Platform("Xbox One X", 2019, "Microsoft", 64, 9, true, 1000, true,"cd", "black" )
+let platform3 = new Platform("PC - Microsoft Windows 10", 2015, "microsoft", 32 || 64, 0 ,true, 0, true, "cd", "black")
+
+let videogame = new Videogame("Hollow Knight", 2017,[person, person6], "australiana", person2, 
+                            ["inglés", "español", "francés", "italiano", "japonés"],[platform3,platform2], 25, 9)
+let videogame2 = new Videogame("The Witcher III", 2015, [person,person6], "polaca", person2, 
+                                ["polaco", "inglés", "español","italiano","japonés","coreano"],[platform,platform2,platform3],30,9)
+let videogame3 = new Videogame("Frozen Flame", 2022, [person6], "británica",person2,["inglés"], [platform3],15,8)
+
+let hobbyGame = new HobbyGame([videogame,videogame2,videogame3])
+
+console.log(hobbyGame.priceTotalPlatform(platform2));
+console.log(hobbyGame.priceTotal());
+console.log(hobbyGame.videogameLanguage("español"));
+console.log((hobbyGame.videogameLanguage2("español")));
+console.log((hobbyGame.videogameDevelop(person)));
+console.log(hobbyGame.videogamePlatLang(platform3,"inglés"));
+console.log(hobbyGame.videogameScore());
+
+
+
+
+
+
+
+
